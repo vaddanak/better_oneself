7eae61311b05ba5bf656905d862a1131123ea750
Vad Seng <vaddanak@gmail.com>
2022-09-16 21:55:43 -0400

    create sample code page

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,9 +4,20 @@
 
 const btn = document.getElementById("show-me");
 const div = document.getElementById("msg");
+const iframe = document.getElementById("code-display");
 let count = 0;
 btn.addEventListener("click", function(event){
-    
-    div.innerHTML = ++count;
 
+    //div.innerHTML = ++count;
+
+    iframe.src="resources/code_pages/code_1.html";
+
+    //iframe.height = iframe.contentWindow.document.body.scrollHeight;
+    
+
+});
+
+iframe.addEventListener("load", function(event){
+
+    console.log("LOAD", iframe.contentWindow.document);
 });
