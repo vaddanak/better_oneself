6a3914d433e9b8d52210923f23382baf59795460
Vad Seng <vaddanak@gmail.com>
2022-09-19 20:48:00 -0400

    add some programs

---
+++ resources/js/code_pages.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let code_pages = [
+
+
+
+
+    {   filename: "resources/code_pages/code_3.html",       
+        name: "permutations",
+        description: "Develop a recursive function to generate all n! permutations of a set of n elements."
+    },
+
+
+
+    {   filename: "resources/code_pages/code_2.html",       
+        name: "magic square",
+        description: "Construct an n x n magic square for any odd integer n."
+    },
+
+
+
+    {   filename: "resources/code_pages/code_1.html",
+        name: "fibonacci",
+        description: "Given a number N, print out the first N numbers in the Fibonacci sequence. "
+    },
+
+    
+
+];
--- resources/js/index.js
+++ resources/js/index.js
@@ -2,22 +2,44 @@
 
 
 
-const btn = document.getElementById("show-me");
-const div = document.getElementById("msg");
+//const btn = document.getElementById("show-me");
+//const div = document.getElementById("msg");
 const iframe = document.getElementById("code-display");
-let count = 0;
-btn.addEventListener("click", function(event){
 
-    //div.innerHTML = ++count;
+const leftSection = document.getElementById("left-section");
 
-    iframe.src="resources/code_pages/code_1.html";
+code_pages.forEach( (e,i,r) => {
+    const pageBtn = document.createElement("button");
+    pageBtn.innerText = e.name;
+    
+    pageBtn.addEventListener("click", function(event){
+        iframe.src = e.filename;
 
-    //iframe.height = iframe.contentWindow.document.body.scrollHeight;
+        const lastActiveButton = document.querySelector("button.active");
+        if( lastActiveButton )
+            lastActiveButton.classList.remove("active");
+
+        event.target.classList.add("active");
+
+    });
+
+    
+    
     
 
+    leftSection.appendChild(pageBtn);
+
+    
 });
 
-iframe.addEventListener("load", function(event){
 
-    console.log("LOAD", iframe.contentWindow.document);
+document.querySelector("#left-section > button:first-child").click();
+
+
+iframe.addEventListener("load", function(event){    
+
+    //iframe.style.width = (iframe.contentWindow.document.body.scrollWidth + 20) + "px";
+    iframe.style.height = (iframe.contentWindow.document.body.scrollHeight + 35) + "px";
+
+    console.log(`IFRAME LOAD width:${iframe.style.width} height:${iframe.style.height}`);
 });
